5ed707507c8ccdd7bbb647ac5830364a871916d9
MluisaGP <luisagalazmp@gmail.com>
2025-09-04 18:38:08 -0700

    Se agregan las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 import Player from "./pages/Player";
 
 function App() {
